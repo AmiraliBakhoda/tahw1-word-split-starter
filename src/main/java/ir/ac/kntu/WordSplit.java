8f5e6bcb0b143e1d39f725843c0326570b409cf9
Amirali Bakhoda <46648466+AmiraliBakhoda@users.noreply.github.com>
2019-03-01 19:30:51 +0330

    Update WordSplit.java

--- src/main/java/ir/ac/kntu/WordSplit.java
+++ src/main/java/ir/ac/kntu/WordSplit.java
@@ -1,14 +1,24 @@
 package ir.ac.kntu;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
-public class WordSplit{
+public class WordSplit {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        //write your logic here
+		input = input.trim();
+		
+		char[] arrayOfInput = input.toCharArray();
+		for (int i=0 ; i < arrayOfInput.length; i++) {
+			if (arrayOfInput[i] == ' ') {
+				arrayOfInput[i] = ',';
+			}
+		}
+		System.out.println(new String(arrayOfInput));
+		
         scanner.close();
-    }
-
+	}
+	
 }
